ec70e2cef52b16262b2406c06931da50e05fa770
kientndev <kien.eat.pizza@gmail.com>
2026-08-05 08:06:19 +0700

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,8 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
+      style={{ colorScheme: 'dark' }}
     >
       <body className="min-h-full flex flex-col bg-zinc-950 text-zinc-150">
         <Providers>
